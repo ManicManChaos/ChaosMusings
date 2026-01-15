b65af6398e7afa63b7eab09e32e5853e35664854
ManicManChaos <eugenedidit8@gmail.com>
2026-01-14 19:05:17 -0600

    Delete components directory

--- components/ui/Glyph.js
+++
@@ -1,7 +0,0 @@
-export default function Glyph({ name, label }) {
-  return (
-    <button className="runeBtn" type="button" aria-label={label}>
-      <img className="rune" src={`/icons/${name}.svg`} alt="" />
-    </button>
-  );
-}
